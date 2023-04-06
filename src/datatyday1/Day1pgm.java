cb36bfabbd6663b1d801c70f1bdca2b18c962277
ABIRAMIRAVICHANDARANravi <abiramiravichandaran@gmail.com>
2023-04-06 20:55:01 +0530

    package and name creation
    This program i created a new package and class for exixting project in this repository

---
+++ src/datatyday1/Day1pgm.java
@@ -0,0 +1,11 @@
+package datatyday1;
+
+public class Day1pgm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	System.out.println("Creation of package and class and Datatype");	
+
+	}
+
+}
