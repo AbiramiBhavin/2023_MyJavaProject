4d11f75a5c57ebdade7e688cb05403bb16cb24cd
ABIRAMIRAVICHANDARANravi <abiramiravichandaran@gmail.com>
2023-04-07 20:46:55 +0530

    tried different datatype conversions and characters
    typecast
    tried which stores what

---
+++ src/datatyday2/Day2prac.java
@@ -0,0 +1,42 @@
+package datatyday2;
+
+public class Day2prac {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean a=true;
+		System.out.println(a);
+		a=false;
+		System.out.println(a);
+		byte b=-126;
+		System.out.println(b);
+		char c='p', d='\\', e='$', f='\n', g='\u0041';
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+		short a1=-32768;
+		System.out.println(a1);
+		int a2=12345678;
+		System.out.println(a2);
+		a2=a1;
+		System.out.println(a2);;
+		long i=15632l;
+		System.out.println(i);
+		float j=2.35621f;
+		System.out.println(j);
+		j=i;
+		System.out.println(j);
+		double p=1e1d;
+		System.out.println(p);
+		p=a2;
+		System.out.println(p);
+		
+		
+		
+		
+
+	}
+
+}
