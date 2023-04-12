7fe83b2c47104dfb37a1b2d29950e02bbedf7787
ABIRAMIRAVICHANDARANravi <abiramiravichandaran@gmail.com>
2023-04-12 20:52:08 +0530

    Overloading with diff argument and access specifier
    // Here overloaded CubeVol method with different arguments and different access specifier and accessed non static through object

---
+++ src/day5Overload/PgmOverlod.java
@@ -0,0 +1,25 @@
+package day5Overload;
+
+public class PgmOverlod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	PgmOverlod p=new PgmOverlod();
+	cubeVol(3);
+	p.cubeVol(9,6);
+
+	}
+	public static void cubeVol(int a)
+	{
+		System.out.println("Volume of cube:");
+		System.out.println(a*a*a);
+	}// Here overloaded CubeVol method with different arguments and different access specifier and accessed non static through object
+	protected void cubeVol(int a,int b) 
+	{
+		System.out.println("Volume of cube:");
+		System.out.println(a*a*a);
+	}
+	
+
+}
