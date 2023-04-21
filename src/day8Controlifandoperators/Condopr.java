5df60618f5702d3ef82aa5613348499cb983815e
ABIRAMIRAVICHANDARANravi <abiramiravichandaran@gmail.com>
2023-04-21 20:50:22 +0530

    conditionalopr

---
+++ src/day8Controlifandoperators/Condopr.java
@@ -0,0 +1,30 @@
+package day8Controlifandoperators;
+
+public class Condopr {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=2;
+		int b=3;
+		boolean c=true;
+		boolean d=false;
+		if(a>=b)
+		{
+			System.out.println("A is big");
+		}
+		else
+		{
+			System.out.println("B is big");
+		}
+		if(c==d)
+		{
+			System.out.println("Both are Equal");
+		}
+		else 
+		{
+			System.out.println("Not equal");
+		}
+
+	}
+
+}
