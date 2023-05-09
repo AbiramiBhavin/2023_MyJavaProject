85c2a76e7d046d30b288e8cdaf43371d1b95fbd2
ABIRAMIRAVICHANDARANravi <abiramiravichandaran@gmail.com>
2023-05-09 21:28:11 +0530

    for loop exampkle
    example of for loop

---
+++ src/day13forloop/forloopeg.java
@@ -0,0 +1,22 @@
+package day13forloop;
+
+public class forloopeg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+// This is the example pgm for for loop
+		//for loop does everything as while but in for loop te actions takes place in singlke line
+		// initialization,condition and increment
+		int i=1;
+		for(i=1;i<=3;i++)// incremental way
+		{
+			System.out.println("Example for for loop");
+		}
+		// the above loop displays the op three times.
+		for(int b=5;b>=1;b--)
+		{
+			System.out.println("This is a decremental way of executing the loop");// this loop is a decremntal way of executing the loop
+		}
+	}
+
+}
