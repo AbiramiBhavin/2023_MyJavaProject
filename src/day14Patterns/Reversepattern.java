9a87046a4ae4c1832cc814ae43f19040ff86e23a
ABIRAMIRAVICHANDARANravi <abiramiravichandaran@gmail.com>
2023-05-09 21:31:09 +0530

    Rpattern
    12345
    		 * 1234
    		 * 123
    		 * 12
    		 * 1

---
+++ src/day14Patterns/Reversepattern.java
@@ -0,0 +1,23 @@
+package day14Patterns;
+
+public class Reversepattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*12345
+		 * 1234
+		 * 123
+		 * 12
+		 * 1
+		 */
+for(int x=5;x>=1;x--)
+{
+	for(int a=1;a<=x;a++)
+	{
+		System.out.print(a);
+	}
+	System.out.println();
+}
+	}
+
+}
